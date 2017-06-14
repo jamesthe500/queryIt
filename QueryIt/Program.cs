17450bff6cd7d31aedf560fe6a796fd13fb0f259
Daniels <Daniels.James@mayo.edu>
2017-06-14 18:53:04 -0500

    gets more methods of the database set up. m5c5

--- QueryIt/Program.cs
+++ QueryIt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,32 @@ namespace QueryIt
     {
         static void Main(string[] args)
         {
-            // one requirement is to use an actual database.
-            // for this we are downloading and intalling the Entity Framework.
-            // right click on References.cs
-            // "manage NuGet Packages..."
-            // find and install EntityFramework. The stable version.
+            // tells the entity framework to drop and recreate the DB everytime the application runs.
+            // simpler that way.
+            Database.SetInitializer(new DropCreateDatabaseAlways<EmployeeDb>());
 
+            // this is a repository for employees. 
+            // it is created by constructing the concrete type SqlRepository<employee>
+            // a SqlRepositry needs a DbContext instance. Gives enough info to create teh schema 
+            // wrapped inusing because the object is disposable. 
+            using (IRepository<Employee> employeeRepository = new SqlRepository<Employee>(new EmployeeDb()))
+            {
+                AddEmployees(employeeRepository);
+                CountEmployees(employeeRepository);
+
+            }
+        }
+
+        private static void CountEmployees(IRepository<Employee> employeeRepository)
+        {
+            Console.WriteLine(employeeRepository.FindAll().Count());
+        }
+
+        private static void AddEmployees(IRepository<Employee> employeeRepository)
+        {
+            employeeRepository.Add(new Employee { Name = "Scott" });
+            employeeRepository.Add(new Employee { Name = "Joseph"});
+            employeeRepository.Commit();        
         }
     }
 }
